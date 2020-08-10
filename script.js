70ba7358a7fe61909cbebf3a883fca634ca7a70b
Davron <example@gmail.com>
2020-08-10 22:02:53 +0300

    first app

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console. log(1);
+const numberofMovies = +prompt('How many movies you have seen?', '' );
+
+const personalMoviesDB = {
+    count: numberofMovies,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false 
+};
+
+const a = prompt('Name of last movie you have seen', ''),
+    b = prompt('how much would you rate it', ''),
+    c = prompt('Name of last movie you have seen', ''),
+    d = prompt('how much would you rate it', '');
+
+personalMoviesDB.movies[a] = b;
+personalMoviesDB.movies[c] = d;
+
+console.log(personalMoviesDB);
